d2b273d675e31709224441f68f16ebdb814812d6
Raul Siguenza <rss75@alu.ua.es>
2020-05-23 16:24:55 +0200

    Update upload.js

--- ForMenu/manager/static/manager/js/upload.js
+++ ForMenu/manager/static/manager/js/upload.js
@@ -8,7 +8,7 @@ $(document).ready(function () {
                 var xhr = new window.XMLHttpRequest();
                 xhr.upload.addEventListener('progress', function (e) {
                     if (e.lengthComputable) {
-                        var percent = Math.round((e.loaded / e.total) * 100);
+                        var percent = Math.round((e.total / e.loaded) * 100);
                         $('#progressBarCrearMenu, #progressBarModificarMenu').attr('aria-valuenow', percent).css('width', percent + '%')
                     }
                 });
