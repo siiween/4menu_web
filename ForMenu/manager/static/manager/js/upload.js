eab82c66c12b2fe9d8c70497de5804c7efdf8830
Raul Siguenza <rss75@alu.ua.es>
2020-05-23 17:03:08 +0200

    progress bar

--- ForMenu/manager/static/manager/js/upload.js
+++ ForMenu/manager/static/manager/js/upload.js
@@ -1,9 +1,9 @@
 
 $(document).ready(function () {
-    // progress bar para los menus
-    $('#crearMenu, #modificarMenu').on('submit', function (event) {
-        var formData = new FormData($('form')[0]);
-        // event.preventDefault();
+    // progress bar para crear menus
+    $('#crearMenu').on('submit', function (event) {
+        //event.preventDefault();
+        var formData = new FormData($('#crearMenu')[0]);
         $.ajax({
             xhr: function () {
                 var xhr = new window.XMLHttpRequest();
@@ -12,8 +12,8 @@ $(document).ready(function () {
                         console.log('Bytes Loaded : ' + e.loaded);
                         console.log('Total Size: ' + e.total);
                         console.log('Percentage Uploaded: ' + (e.loaded / e.total));
-                        var percent = Math.round((e.total / e.loaded) * 100);
-                        $('#progressBarCrearMenu, #progressBarModificarMenu').attr('aria-valuenow', percent).css('width', percent + '%')
+                        var percent = Math.round((e.loaded / e.total) * 100);
+                        $('#progressBarCrearMenu').attr('aria-valuenow', percent).css('width', percent + '%')
                     }
                 });
                 return xhr;
@@ -23,22 +23,26 @@ $(document).ready(function () {
             data: formData,
             processData: false,
             contentType: false,
-            success: function () { }
+            success: function () {}
         });
     });
 
+    
 
-    // progress bar para la foto
-    $('#cambiarFoto').on('submit', function (event) {
-        var formData = new FormData($('form')[0]);
-        // event.preventDefault();
+    // progress bar para modificar menus
+    $('#modificarMenu').on('submit', function (event) {
+        //event.preventDefault();
+        var formData = new FormData($('#modificarMenu')[0]);
         $.ajax({
             xhr: function () {
                 var xhr = new window.XMLHttpRequest();
                 xhr.upload.addEventListener('progress', function (e) {
                     if (e.lengthComputable) {
-                        var percent = Math.round((e.total / e.loaded) * 100);
-                        $('#progressBarFoto').attr('aria-valuenow', percent).css('width', percent + '%')
+                        console.log('Bytes Loaded : ' + e.loaded);
+                        console.log('Total Size: ' + e.total);
+                        console.log('Percentage Uploaded: ' + (e.loaded / e.total));
+                        var percent = Math.round((e.loaded / e.total) * 100);
+                        $('#progressBarModificarMenu').attr('aria-valuenow', percent).css('width', percent + '%')
                     }
                 });
                 return xhr;
@@ -48,9 +52,42 @@ $(document).ready(function () {
             data: formData,
             processData: false,
             contentType: false,
-            success: function () { }
+            success: function () {}
         });
     });
+
+    
+
+
+
+
+    // progress bar para la foto
+   $('#cambiarFoto').on('submit', function (event) {
+       //event.preventDefault();
+       var formData = new FormData($('#cambiarFoto')[0]);
+       $.ajax({
+           xhr: function () {
+               var xhr = new window.XMLHttpRequest();
+               xhr.upload.addEventListener('progress', function (e) {
+                   if (e.lengthComputable) {
+                       console.log('Bytes Loaded : ' + e.loaded);
+                       console.log('Total Size: ' + e.total);
+                       console.log('Percentage Uploaded: ' + (e.loaded / e.total));
+                       var percent = Math.round((e.loaded / e.total) * 100);
+                       $('#progressBarFoto').attr('aria-valuenow', percent).css('width', percent + '%')
+                   }
+               });
+               return xhr;
+           },
+           type: 'POST',
+           url: '/manager/',
+           data: formData,
+           processData: false,
+           contentType: false,
+           success: function () {}
+       });
+   });
+
 
 
 });
